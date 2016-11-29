ffec4bbc7fa3a0bae932be9d9fdea4f0d2302762
iOS_Bacon <lipeigen@nxin.com>
2016-11-29 15:41:46 +0800

    init submit

---
+++ tryFunNew/tryFunNew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tryFunNew
+//
+//  Created by iOSBacon on 2016/11/29.
+//  Copyright © 2016年 iOSBacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tryFunNew/tryFunNew/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tryFunNew
+//
+//  Created by iOSBacon on 2016/11/29.
+//  Copyright © 2016年 iOSBacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
